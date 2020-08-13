e0c71d91f25137b90ffd10fa62d3b4a8a49e50a0
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-13 08:50:18 -0700

    Update server.js

--- Web-app/server.js
+++ Web-app/server.js
@@ -1,38 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const app = express()
 
-const apiKey = 'c2538ea856da5fe4035b11811cb61ad0';
-
-app.use(express.static('public'));
+app.use(express.static('/tmp/NODEAPP/Web-app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  res.render('/tmp/NODEAPP/Web-app/views/index');
 })
-
 app.post('/', function (req, res) {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-  console.log(req.body.city)
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
-      let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } else {
-        let weatherText = `It's ${weather.main.temp} degrees with ${weather.weather[0].main} in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-        console.log("body:", body)
-      }
-    }
-  });
+  console.log(req.body.city);
+  res.render('/tmp/NODEAPP/Web-app/index');  
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
 
-app.listen(3000, function () {
-  console.log('Weatherly app listening on port 3000!')
-})
