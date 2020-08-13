d00aeef8ec4b23f486b94bb281380620cbec4a0d
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-13 05:27:00 -0700

    Update server.js

--- Web-app/server.js
+++ Web-app/server.js
@@ -5,12 +5,12 @@ const app = express()
  
 const apiKey = 'c2538ea856da5fe4035b11811cb61ad0';
  
-app.use(express.static('public'));
+app.use(express.static('/tmp/NODEAPP/Web-app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
  
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  res.render('/tmp/NODEAPP/Web-app/views/index', {weather: null, error: null});
 })
  
 app.post('/', function (req, res) {
@@ -19,14 +19,14 @@ app.post('/', function (req, res) {
   console.log(req.body.city)
   request(url, function (err, response, body) {
     if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+      res.render('/tmp/NODEAPP/Web-app/views/index', {weather: null, error: 'Error, please try again'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+        res.render('/tmp/NODEAPP/Web-app/views/index', {weather: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees with ${weather.weather[0].main} in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        res.render('/tmp/NODEAPP/Web-app/views/index', {weather: weatherText, error: null});
         console.log("body:", body)
       }
     }
